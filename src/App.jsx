53468d6ed7290bfb335f98511a704c8e9bf848f6
Alby Mathews Bruse <albymbruse@gmail.com>
2025-05-04 11:02:27 -0400

    yaboy did sm work fr

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ function App() {
      <h1>Reach69.IO</h1>
      <h2>Reach 69 by creating equations with the given digits</h2>
 
-    <div className='btns'>
-      <button type="button" class="btn btn-outline-dark">Log In</button>
-      <button type="button" class="btn btn-outline-dark">Log In</button>
-      <button type="button" class="btn btn-outline-dark">Log In</button>
+    <div className='btnRow'>
+      <button type="button" id='logInBtn' class="btn btn-outline-dark">Log In</button>
+      <button type="button" id='playBtn' class="btn btn-dark">Play</button>
+      <button type="button" id='modesBtn' class="btn btn-outline-dark">Modes</button>
      </div>
      
     </>
