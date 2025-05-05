d3f3502e8c0513f74a7f58b726868ee7eb3dd054
itsrihan <110xrihan@gmail.com>
2025-05-05 19:12:30 +0400

    level1 basics, single page aki

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import Level1 from './Level1';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [page, setPage] = useState('home');
 
   return (
     <>
-     <div className='logo'>
+      {page === 'home' && (
+        <div>
+          <h1>Reach69.IO</h1>
+          <h2>Reach 69 by creating equations with the given digits</h2>
+          <div className="btnRow">
+            <button onClick={() => setPage('login')} className="btn btn-outline-dark">Log In</button>
+            <button onClick={() => setPage('play')} className="btn btn-dark">Play</button>
+            <button onClick={() => setPage('modes')} className="btn btn-outline-dark">Modes</button>
+          </div>
+        </div>
+      )}
 
-     </div>
-     <h1>Reach69.IO</h1>
-     <h2>Reach 69 by creating equations with the given digits</h2>
-
-    <div className='btnRow'>
-      <button type="button" id='logInBtn' class="btn btn-outline-dark">Log In</button>
-
-      {/* idk onclick and this window location href work avulu ig, coz button class */}
-      <button type="button" id='playBtn' class="btn btn-dark" onClick={() => window.location.href = "level1.jsx"}>Play</button>
-      <button type="button" id='modesBtn' class="btn btn-outline-dark">Modes</button>
-     </div>
-     
+      {page === 'play' && <Level1 />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/level1.jsx
+++ src/level1.jsx
@@ -1,6 +1,23 @@
 // oh demn inganyum comment idam 
 // for now level 1 irikatte ig, 
 
-function MyComponent() {
-    
+const Level1 = () => {
+    return (
+        <>
+        <div className="puzzle">
+            <div className="level1">Level 1</div>
+        
+            <div className="number">
+                <div className="7"> 7 </div>
+                <div className="1"> 1 </div>
+                <div className="2"> 2 </div>
+                <div className="0"> 0 </div>
+                <div className="2"> 2 </div>
+            </div>
+        </div>
+        </>
+    )
+
 }
+
+export default Level1;
